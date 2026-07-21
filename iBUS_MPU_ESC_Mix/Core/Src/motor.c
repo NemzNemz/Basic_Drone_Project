bb9bfca4ddeecbf26b04fec388fd34f6abac456b
NemzNemz <urgot0933@gmail.com>
2026-07-21 18:54:30 +0700

    feat(motor): Thêm mới hàm cập nhật struct lưu giữ giá trị pwm 4 motor, thêm mới 2 define set ngưỡng pwm tối thiểu và tối đa

--- iBUS_MPU_ESC_Mix/Core/Src/motor.c
+++ iBUS_MPU_ESC_Mix/Core/Src/motor.c
@@ -2,7 +2,7 @@
  * motor.c
  *
  *  Created on: Jul 13, 2026
- *      Author: ADMIN
+ *      Author: Nemz
  */
 
 #include "motor.h"
@@ -16,11 +16,11 @@ void Motor_Init(TIM_HandleTypeDef *hw_esc_htim){
 	assert(esc_htim != NULL);
 }
 
-void Motor_Set_Speed(uint16_t speed){
-	esc_htim->Instance->CCR1 = speed;
-	esc_htim->Instance->CCR2 = speed;
-	esc_htim->Instance->CCR3 = speed;
-	esc_htim->Instance->CCR4 = speed;
+void Motor_Set_Speed(MOTOR *mt_ptr){
+	esc_htim->Instance->CCR1 = mt_ptr->m1;
+	esc_htim->Instance->CCR2 = mt_ptr->m2;
+	esc_htim->Instance->CCR3 = mt_ptr->m3;
+	esc_htim->Instance->CCR4 = mt_ptr->m4;
 }
 
 void Motor_Lock(TIM_HandleTypeDef *hw_esc_htim){
@@ -36,5 +36,46 @@ void Motor_Min_Throttle(TIM_HandleTypeDef *hw_esc_htim){
 	esc_htim->Instance->CCR3 = 13000;
 	esc_htim->Instance->CCR4 = 13000;
 }
+
+void Motor_Update_Values(MOTOR *mt_ptr, uint16_t m1, uint16_t m2, uint16_t m3, uint16_t m4) {
+    // Ép biên cho 4 động cơ. Đây là M1
+    if(m1 > MOTOR_MAX_PWM){
+        mt_ptr->m1 = MOTOR_MAX_PWM;
+    }
+    else if(m1 < MOTOR_MIN_PWM){
+        mt_ptr->m1 = MOTOR_MIN_PWM;
+    }
+    else mt_ptr->m1 = m1;
+
+    //M2
+    if(m2 > MOTOR_MAX_PWM){
+        mt_ptr->m2 = MOTOR_MAX_PWM;
+    }
+    else if(m2 < MOTOR_MIN_PWM){
+        mt_ptr->m2 = MOTOR_MIN_PWM;
+    }
+    else mt_ptr->m2 = m2;
+
+    //M3
+    if(m3 > MOTOR_MAX_PWM){
+        mt_ptr->m3 = MOTOR_MAX_PWM;
+    }
+    else if(m3 < MOTOR_MIN_PWM){
+        mt_ptr->m3 = MOTOR_MIN_PWM;
+    }
+    else  mt_ptr->m3 = m3;
+
+    //M4
+    if(m4 > MOTOR_MAX_PWM){
+        mt_ptr->m4 = MOTOR_MAX_PWM;
+    }
+    else if(m4 < MOTOR_MIN_PWM){
+        mt_ptr->m4 = MOTOR_MIN_PWM;
+    }
+    else mt_ptr->m4 = m4;
+}
+
+
+
 
 
