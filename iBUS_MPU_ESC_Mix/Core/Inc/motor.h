3060b3ba4023f94525ce9e95c8bb7c0479898183
NemzNemz <urgot0933@gmail.com>
2026-07-21 18:54:14 +0700

    feat(motor): Thêm mới hàm cập nhật struct lưu giữ giá trị pwm 4 motor, thêm mới 2 define set ngưỡng pwm tối thiểu và tối đa

--- iBUS_MPU_ESC_Mix/Core/Inc/motor.h
+++ iBUS_MPU_ESC_Mix/Core/Inc/motor.h
@@ -2,17 +2,26 @@
  * motor.h
  *
  *  Created on: Jul 13, 2026
- *      Author: ADMIN
+ *      Author: Nemz
  */
-
 #ifndef INC_MOTOR_H_
 #define INC_MOTOR_H_
+#define MOTOR_MIN_PWM 13000
+#define MOTOR_MAX_PWM 25000
 
 #include "main.h"
-void Motor_Set_Speed(uint16_t speed);
+
+typedef struct{
+	uint16_t m1;
+	uint16_t m2;
+	uint16_t m3;
+	uint16_t m4;
+}MOTOR;
+
+void Motor_Update_Values(MOTOR *mt_ptr, uint16_t m1, uint16_t m2, uint16_t m3, uint16_t m4);
+void Motor_Set_Speed(MOTOR *mt_ptr);
 void Motor_Init(TIM_HandleTypeDef *hw_esc_htim);
 void Motor_Lock(TIM_HandleTypeDef *hw_esc_htim);
 void Motor_Min_Throttle(TIM_HandleTypeDef *hw_esc_htim);
-
 
 #endif /* INC_MOTOR_H_ */
