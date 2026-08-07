7ca94b37cba526bdf00a29688cb58a2cd7f97336
NemzNemz <urgot0933@gmail.com>
2026-08-07 19:47:03 +0700

    feat(bat): tinh chỉnh nhẹ tham số bộ chia áp

--- iBUS_MPU_ESC_Mix/Core/Inc/battery.h
+++ iBUS_MPU_ESC_Mix/Core/Inc/battery.h
@@ -9,8 +9,8 @@
 #define INC_BATTERY_H_
 
 #include "main.h"
-// 4S: 3.3/4096 * 6 (R4=1M, R5=200k) = 0.00483396f
-#define BAT_CALIB_FACTOR 	0.00080566f //0.00483396f
+// 4S: 3.3/4096 * 6 (R4=820k, R5=200k) = 0.004108887f
+#define BAT_CALIB_FACTOR 	0.004576857f //0.00483396f
 
 void BAT_INIT(ADC_HandleTypeDef *bat_hadc, uint32_t *pData);
 void BAT_GET_VOL(uint32_t raw_adc_vl, float *bal_ptr);
