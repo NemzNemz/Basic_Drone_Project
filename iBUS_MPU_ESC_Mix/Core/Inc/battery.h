359d35d976602a3a5462874ad41a916c92274d96
NemzNemz <urgot0933@gmail.com>
2026-09-09 20:06:26 +0700

    Sửa tham số CALIB 1 lần nữa

--- iBUS_MPU_ESC_Mix/Core/Inc/battery.h
+++ iBUS_MPU_ESC_Mix/Core/Inc/battery.h
@@ -9,9 +9,8 @@
 #define INC_BATTERY_H_
 
 #include "main.h"
-// Cầu phân áp: R1 = 820k, R3 = 200k (Tỷ lệ chia áp = 5.1)
-// Điện áp pin = RAW_ADC * (3.3 / 4096) * 5.1
-#define BAT_CALIB_FACTOR 	0.01071167f
+//15.4V lúc đo pin / raw_adc  ≈ 0.01363f
+#define BAT_CALIB_FACTOR 	0.01363f
 
 void BAT_INIT(ADC_HandleTypeDef *bat_hadc, uint32_t *pData);
 void BAT_GET_VOL(uint32_t raw_adc_vl, float *bal_ptr);
