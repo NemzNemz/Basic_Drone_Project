c0ae3491cacf6754fffe469154f32ae6d27fc406
NemzNemz <urgot0933@gmail.com>
2026-09-09 20:07:12 +0700

    đổi dạng ngắt (đúng hơn là đảo chân nên vector ngắt trỏ tới bị đổi rồi

--- iBUS_MPU_ESC_Mix/Core/Inc/stm32f4xx_it.h
+++ iBUS_MPU_ESC_Mix/Core/Inc/stm32f4xx_it.h
@@ -55,11 +55,11 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-void EXTI2_IRQHandler(void);
+void EXTI1_IRQHandler(void);
 void TIM1_UP_TIM10_IRQHandler(void);
 void TIM1_TRG_COM_TIM11_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
-void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
